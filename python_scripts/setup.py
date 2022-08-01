431e4002fd72df05969d0673b7ac830884477e28
pengzhangzhi <pengzhangzhics@gmail.com>
2022-08-01 11:31:25 +0800

    first commit

---
+++ python_scripts/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+import os
+# make sure you have the file. otherwise do `__version__ = "0.0.0"`
+if os.path.exists('structure_module/version.py'):
+    exec(open('structure_module/version.py').read())
+else:
+    __version__ = "0.0.0"
+
+setup(
+  name = 'structure_module-pytorch',
+  packages = find_packages(exclude=[]),
+  include_package_data = True,
+  entry_points={
+    'console_scripts': [
+      'structure_module = structure_module.cli:main',
+      'test_structure_module = structure_module.cli:test'
+    ],
+  },
+  version = __version__,
+  license='Apache',
+  description = 'pytorch implementation of structure_module of Alphafold2',
+  author = 'Zhangzhi Peng',
+  author_email = 'pengzhangzhics@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/pengzhangzhi/protein_structure_module_of_AF2',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'protein structure prediction'
+  ],
+  install_requires=[
+    # 'einops>=0.4',
+    # 'numpy',
+    # 'packaging',
+    # 'torch>=1.10',
+    # 'tqdm',
+    # "deepspeed",
+    # "biopython",
+    "click >= 8.1.2",
+    # "deepspeed >= 0.6.7",
+    "dm_tree >= 0.1.7",
+    "numpy >= 1.23.0",
+    "scipy >=  1.8.0",
+    "torch >= 1.11.0",
+
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: Apache License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
